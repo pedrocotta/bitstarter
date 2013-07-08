741386b964859ad0e3c7432fc141a4b7a0fed775
Pedro Cotta <pedrocotta@yahoo.com.br>
2013-07-08 22:33:46 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,35 @@
 var express = require('express');
 
+var fs = require ('fs');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+    response.send(readfile('./Index.html'));
+    //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var readfile = function (fileName) {
+    var buffer = fs.readFileSync(fileName);
+    return buffer.toString();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
